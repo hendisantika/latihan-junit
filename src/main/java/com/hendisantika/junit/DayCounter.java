dd810d486cb32ddda7813061b8661cc42a0a05b6
Hendi Santika <hendisantika@yahoo.co.id>
2017-03-28 22:02:32 +0700

    Latihan JUnit - Add required files

---
+++ src/main/java/com/hendisantika/junit/DayCounter.java
@@ -0,0 +1,17 @@
+package com.hendisantika.junit;
+
+/**
+ * Created by hendisantika on 3/28/17.
+ */
+public class DayCounter {
+    /**
+     * menghitung jumlah hari dalam bulan dan tahun yang diminta.
+     * Misalnya, bila month = 2 dan year = 2000, method ini akan menghasilkan 29.
+     * @param month bulan yang akan dihitung
+     * @param year tahun yang akan dihitung
+     * @return jumlah hari dalam bulan dan tahun bersangkutan
+     */
+    public int numDays(int month, int year) {
+        return 29;
+    }
+}
